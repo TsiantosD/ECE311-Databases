9a3a738ac3f28a04996dc59744606e93379510ef
Jason Nikas <nikasjason1@gmail.com>
2025-01-13 21:05:16 +0200

    Update schema removed weak entity from posts

--- backend/django_server/api/urls.py
+++ backend/django_server/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("test", views.index, name="index"),
+    path("post", views.index, name="index"),
+    path("react", views.index, name="index"),
 ]
--- backend/django_server/api/views.py
+++ backend/django_server/api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_http_methods
+from django.db import transaction, connection
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@require_http_methods(["GET", "POST"])
+def post(request):
+    if request.method == "POST":
+        return HttpResponse("POST request received")
+    else:
+        query = "SELECT * FROM Posts;"
+        with transaction.atomic():
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                posts = cursor.fetchall()
+        return JsonResponse(posts, safe=False)
